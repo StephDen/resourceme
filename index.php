23108d2c9a3059e6430f6b79064dc5dfdc4f10cd
edhou <edward.h@live.ca>
2017-11-07 21:33:04 -0500

    Test

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
-    <h1> AESTHETIC EDWARDO</h1>
-    
+      
+    <h1>AESTHETIC <small class="text-muted">TEXT</small></h1>
+    <div class="alert alert-primary" role="alert">
+        Hey, how're you doing?
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
