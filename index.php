b1b89acde48f5c368575fe31f2c611c43d0507d7
Stephen Zhao <stephenzhao123@outlook.com>
2017-11-10 19:35:26 +0000

    ok

--- index.php
+++ index.php
@@ -36,6 +36,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body onload="myFunction()" style="margin:0;">
+
     <div class="loadercontainer" id="loader"></div> 
     
     <div style="display:none;" id="myDiv" class="animate-bottom">
