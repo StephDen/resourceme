71bcf2694fad5fd94b1f98f8b8310dd5485d7d5d
tolllord <xinghaozhaoex@gmail.com>
2017-11-16 18:55:38 -0500

    ajaxtest

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-  //connecting to db
-  require('php/dbconn.php');
-  require('php/cookiefuncs.php');
-  //collecting data from db
-  $time1 = time();
-  $tsql= "INSERT INTO test VALUES (".$time1.");";
-  $getResults= sqlqry($conn, $tsql);
-
-  $tsql= "SELECT * FROM test WHERE test = ".$time1.";";
-  $getResults= sqlqry($conn, $tsql);
-
-  $row = sqlsrv_fetch_array($getResults);
-  $str = $row['test'];
-  $cookiename = 'edwardo';
-  $my_salt = 'SomeRandomString-hY5K92AzVnMYyT7';
-
-  //setting cookie
-  setSafeCookie($cookiename,$my_salt,$str);
-  $encrypted_string = $_COOKIE[$cookiename];
-  $decrypted_string = getSafeCookie($my_salt, $cookiename);
-  //test
-?>
-
-
 <!--HTML SECTION-->
 <html lang="en">
   <head>
@@ -51,12 +26,7 @@
     <div style="display:none;" id="myDiv" class="animate-bottom">
       <h1>AESTHETIC<small class="text-muted"><?php echo $row['test'];?> </small></h1>
       <img src="images/logo.png" alt="logo" class="container">
-      <div class="alert alert-primary" role="alert">
-        <p><?php echo "Encrypted Cookie:".$encrypted_string."<br/>";
-          echo "Decrypted Cookie:".$decrypted_string;?>
-        </p>
-      </div>
-
+      <button type="button" id="btn">INSERT INTO DB</button>
     </div>
       <div id="map"></div>
     <!-- Optional JavaScript -->
@@ -90,7 +60,13 @@
       });
     }
     </script>
-
+    <script type="text/javascript">
+      $(function(){
+        $('#btn').on('click', function(){
+           alert('click event');
+        });
+      });
+    </script>
 
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
---
+++ php/dbwrite.php
@@ -0,0 +1,28 @@
+<?php
+  //connecting to db
+  require('php/dbconn.php');
+  require('php/cookiefuncs.php');
+  //collecting data from db
+  $time1 = time();
+  $tsql= "INSERT INTO test VALUES (".$time1.");";
+  $getResults= sqlqry($conn, $tsql);
+
+  $tsql= "SELECT * FROM test WHERE test = ".$time1.";";
+  $getResults= sqlqry($conn, $tsql);
+
+  $row = sqlsrv_fetch_array($getResults);
+  $str = $row['test'];
+  $cookiename = 'edwardo';
+  $my_salt = 'SomeRandomString-hY5K92AzVnMYyT7';
+
+  //setting cookie
+  setSafeCookie($cookiename,$my_salt,$str);
+  $encrypted_string = $_COOKIE[$cookiename];
+  $decrypted_string = getSafeCookie($my_salt, $cookiename);
+  //test
+?>
+<div class="alert alert-primary" role="alert">
+  <p><?php echo "Encrypted Cookie:".$encrypted_string."<br/>";
+    echo "Decrypted Cookie:".$decrypted_string;?>
+  </p>
+</div>
