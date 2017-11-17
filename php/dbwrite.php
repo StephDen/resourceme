4a410121699fa3ddd8c34c7f794c34eb76d43611
tolllord <xinghaozhaoex@gmail.com>
2017-11-16 21:58:53 -0500

    asdf

--- php/dbwrite.php
+++ php/dbwrite.php
@@ -3,7 +3,7 @@
   require('php/dbconn.php');
   require('php/cookiefuncs.php');
   //collecting data from db
-  $input = $_POST['postsubmit'];
+  $input = $_POST['postcontent'];
   $tsql= "INSERT INTO test VALUES (".$input.");";
   $getResults= sqlqry($conn, $tsql);
 
