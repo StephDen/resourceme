a546e8a67c86ce5e59d282f97a4109ddef510f5d
tolllord <xinghaozhaoex@gmail.com>
2017-11-18 15:01:06 -0500

    test

--- js/gglmps.js
+++ js/gglmps.js
@@ -1,7 +1,7 @@
 function initMap() {
-  var want_loc = {lat:  49.285624 , lng: -123.124662 };
+  var want_loc = {lat: 49.285624 , lng: -123.124662 };
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
+    zoom: 4,
     center: want_loc
   });
   var marker = new google.maps.Marker({
