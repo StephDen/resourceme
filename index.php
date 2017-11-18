fe474ff0989b9f4e797e4ab144efce6b90199b33
tolllord <xinghaozhaoex@gmail.com>
2017-11-18 16:18:42 -0500

    test

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require('php/dbconnect.php');
-
+$tsql = "INSERT INTO test (test) VALUES (5);"
+dbconnect::sqlqry($tsql);
 ?>
 
 <html lang="en">
   <head>
-    <title>Hello, world!</title>
+    <title>ResourceMe</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
