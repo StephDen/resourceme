d9948bc4e2c39a3b2f111ab9765985348515404c
Stephen Zhao <stephenzhao123@outlook.com>
2017-11-18 21:20:41 +0000

    test

---
+++ php/dbconnect.php
@@ -0,0 +1,37 @@
+<?php
+class dbconnect{
+  //server name
+  private $serverName = "resourceme.database.windows.net";
+  //Connection array with log in credentials
+  private $connectionOptions = array(
+      "Database" => "User_Id",
+      "Uid" => "bigadmin",
+      "PWD" => "Qwerty!@#"
+  );
+  //connection variable
+  private $conn = sqlsrv_connect($this->$serverName, $this->$connectionOptions);
+
+  //querying function
+  public static function sqlqry($tsql){
+      $getResults = sqlsrv_query($this->$conn, $tsql);
+      if ($getResults == FALSE){
+          echo (sqlsrv_errors());
+        }
+      $row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)
+      return $row;
+  }
+
+}
+
+
+/*How to query---------------------------------
+$getResults= sqlsrv_query($conn, $tsql);
+
+Catching errors--------------------------------
+if ($getResults == FALSE)
+    echo (sqlsrv_errors());
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+ echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+}
+sqlsrv_free_stmt($getResults);*/
+?>
--- php/getlocdb.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-require('php/dbconn.php');
-//getting the location of SNC Lavalin from db
-$qry = "SELECT lat,lng FROM markers WHERE id = 2;";
-$getResults= sqlqry($conn, $tsql);
-$location = sqlsrv_fetch_array($getResults);
-?>
