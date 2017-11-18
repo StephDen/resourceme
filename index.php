fe39b6761ff0df315108137378bf6d30197730ca
Stephen Zhao <stephenzhao123@outlook.com>
2017-11-18 22:12:42 +0000

    test

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+
+<?php
+  echo 'hello';
+?>
 <html lang="en">
   <head>
     <title>ResourceMe</title>
@@ -10,10 +14,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   </head>
   <body>
-    <h1>
-<?php
-  echo 'hello';
-?></h1>
+    <h1>hi</h1>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
