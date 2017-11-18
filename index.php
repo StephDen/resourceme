bbb5170bee897876d27437195f63eabefd9d2e25
Stephen Zhao <stephenzhao123@outlook.com>
2017-11-18 20:24:45 +0000

    hb

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ require('php/dbwrite.php');
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-     
+
      <!--CSS-->
       <link rel="stylesheet" type="text/css" href="css/gradientanimate.css">
-      <link rel="stylesheet" type="text/css" href="css/loader.css">  
+      <link rel="stylesheet" type="text/css" href="css/loader.css">
   </head>
   <body>
 
@@ -28,7 +28,7 @@ require('php/dbwrite.php');
         </p>
       </div>
       <button onClick="window.location='map.php';" value="click for good job" />
-      
+
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
