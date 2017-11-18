b73cdd0178433562b97019777b35115a9df11e8b
tolllord <xinghaozhaoex@gmail.com>
2017-11-18 15:09:46 -0500

    test

--- php/getlocdb.php
+++ php/getlocdb.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 require('php/dbconn.php');
 //getting the location of SNC Lavalin from db
@@ -6,12 +6,3 @@ $qry = "SELECT lat,lng FROM markers WHERE id = 2;";
 $getResults= sqlqry($conn, $tsql);
 $location = sqlsrv_fetch_array($getResults);
 ?>
-=======
-<?php
-require('php/dbconn.php');
-//getting the location of SNC Lavalin from db
-$qry = "SELECT lat,lng FROM markers WHERE id = 2;";
-$getResults= sqlqry($conn, $tsql);
-$location = sqlsrv_fetch_array($getResults);
-?>
->>>>>>> a546e8a67c86ce5e59d282f97a4109ddef510f5d
