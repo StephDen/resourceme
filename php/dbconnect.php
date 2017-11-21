a91bec8d0b8755cda4a08dd0e484a42a7841cf4f
tolllord <xinghaozhaoex@gmail.com>
2017-11-20 20:21:36 -0500

    asd

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,16 +1,16 @@
 <?php
 class dbconnect {
-  //server name
-  private static $serverName = "blochacks.database.windows.net";
-  //Connection array with log in credentials
-  private static $connectionOptions = array(
-      "Database" => "resources",
-      "Uid" => "bigadmin",
-      "PWD" => "Qwerty!@#"
-  );
 
   //querying function
   public static function sqlqry($tsql){
+      //server name
+      $serverName = "blochacks.database.windows.net";
+      //Connection array with log in credentials
+      $connectionOptions = array(
+          "Database" => "resources",
+          "Uid" => "bigadmin",
+          "PWD" => "Qwerty!@#"
+      );
       $conn = sqlsrv_connect(self::$serverName, self::$connectionOptions);
       $getResults = sqlsrv_query(self::$conn, $tsql);
       echo 'connected';
