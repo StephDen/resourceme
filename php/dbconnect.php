e072683b0e960010d554a90b8d627343f3494df3
tolllord <xinghaozhaoex@gmail.com>
2017-11-20 20:24:39 -0500

    test

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,17 +1,17 @@
 <?php
 class dbconnect {
-
+  //server name
+  private static $serverName = "blochacks.database.windows.net";
+  //Connection array with log in credentials
+  private static $connectionOptions = array(
+      "Database" => "resources",
+      "Uid" => "bigadmin",
+      "PWD" => "Qwerty!@#"
+  );
   //querying function
   public static function sqlqry1(){
-      //server name
-      $serverName = "blochacks.database.windows.net";
-      //Connection array with log in credentials
-      $connectionOptions = array(
-          "Database" => "resources",
-          "Uid" => "bigadmin",
-          "PWD" => "Qwerty!@#"
-      );
-      $conn = sqlsrv_connect($serverName,$connectionOptions);
+
+      $conn = sqlsrv_connect(self::$serverName,self::$connectionOptions);
       $getResults = sqlsrv_query($conn, $tsql);
       echo 'connected';
       if ($getResults == FALSE){
