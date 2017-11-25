27d3438ccb1d53ceaba0b5a8ca56351cb006d1db
Alex <alexb.bless915@gmail.com>
2017-11-25 17:35:48 -0500

    New table and fixes on status table

--- SQL/Create Table/Status.sql
+++ SQL/Create Table/Status.sql
@@ -1,7 +1,7 @@
 --Code for creating tables just in case
 Create TABLE Status(
-    ID char             Not Null,
-    Date_Updated text,
+    ID char (16)          Not Null,
+    Date_Updated date,
     Type_of_Process text,
     Process_Status text,
     Primary Key (ID)
---
+++ SQL/Create Table/location.sql
@@ -0,0 +1,9 @@
+--Code for creating tables just in case
+CREATE TABLE Location(
+    ID char(16)     Not Null,
+    Date_Arrived Date,
+    Country char(30),
+    City char(30),
+    Street_Address text,
+    Primary Key(ID)
+);
