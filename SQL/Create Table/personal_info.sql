47a622afcd651e60b8af33ce9f466d59f951f8ab
Alex <alexb.bless915@gmail.com>
2017-11-25 18:26:16 -0500

    New Personal Info Table

---
+++ SQL/Create Table/personal_info.sql
@@ -0,0 +1,20 @@
+--Code for creating tables just in case
+CREATE TABLE Personal_Info(
+    ID char(16) Not Null,
+    Full_Name char(50),
+    Date_of_Birth Date,
+    Sex char(15),
+    Marital_Status char(15),
+    Religion char(30),
+    Citizenship char (40),
+    Photograph text,
+    Place_of_Birth char(40),
+    Occupation char(30),
+    Education_Level char(40),
+    Ethnic_Origin char(50),
+    Last_Updated datetime,
+    Updated_By char(50),
+    Last_Login datetime,
+    Active_Status char(4),
+    Primary Key(ID)
+);
