b12ee8a3d9672eaa7558fe29f07bcebe27d82cb3
edhou <edward.h@live.ca>
2017-11-26 00:42:50 -0500

    php testing

--- profile.php
+++ profile.php
@@ -1183,8 +1183,16 @@ $php_array = dbconnect::sql_query('SELECT * FROM test');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
       
-    <script src="/js/register_onload.js"></script>
+    
     <script>
+    
+    function register_onload(){
+    var js_array = JSON.parse( '<?php echo json_encode($php_array) ?>');
+    /*HOW to access array in JS
+    * console.log( js_array['vegetables']['leafy'][0] ); // collard greens
+    */
+}
+        
       document.getElementById('inputGivenName').value = console.log( js_array[0][0] );  
     </script>
   </body>
