7258fbbd4f600468b65293a1f199805fc98afc53
tolllord <xinghaozhaoex@gmail.com>
2017-11-25 23:46:26 -0500

    changing register on load.js

--- js/register_onload.js
+++ js/register_onload.js
@@ -1,4 +1,4 @@
-function register_onload() {
+function register_onload(){
     var js_array = JSON.parse( '<?php echo json_encode($php_array) ?>');
     /*HOW to access array in JS
     * console.log( js_array['vegetables']['leafy'][0] ); // collard greens
