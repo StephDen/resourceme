19a14d0dac6c50015bad544085fc7855ff0d0569
tolllord <xinghaozhaoex@gmail.com>
2017-11-26 10:12:21 -0500

    create account

---
+++ createuser.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 2017-11-26
+ * Time: 10:07 AM
+ */
+
+$user = htmlspecialchars($_POST["username"]);
+$pass = htmlspecialchars($_POST["password"]);
+$token = tokenID::hardcodecookie();
+GenerateQuery::Create_New_User($user,$pass,$token);
+setcookie('username',
+    $user,
+    time()+(86400 * 30)
+);//time = 30days
+header('Location: profile.php');
+?>
