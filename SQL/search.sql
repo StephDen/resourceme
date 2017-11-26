a1bed2385db26f586b02e42e6330c9340259b62f
Alex <alexb.bless915@gmail.com>
2017-11-26 09:53:49 -0500

    Fixed Search query

--- SQL/search.sql
+++ SQL/search.sql
@@ -1,9 +1,9 @@
 SELECT First_Name, Last_Name, Phone, Email, Sex, Marital_Status, Education_Level, Country_of_Birth, Country_of_Origin, Special_Needs
 FROM Personal_Info
-WHERE Phone = @phone AND
-Email = @email AND
-Sex = @sex AND
-Marital_Status = @marital_status AND
-Education_Level = @education_level AND
-Country_of_Birth = @country_of_birth AND
-Special_Needs = @special_needs; 
+WHERE Phone = @phone OR @phone=Null AND
+Email = @email OR @email=Null AND
+Sex = @sex OR @sex=Null AND
+Marital_Status = @marital_status OR @marital_status = Null AND
+Education_Level = @education_level OR @education_level = Null AND
+Country_of_Birth = @country_of_birth OR @country_of_birth = Null AND
+Special_Needs = @special_needs OR @special_needs = Null; 
