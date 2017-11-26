90f997f3931100703912fdc5c4b087b756f4d18e
Victorloyf <33768636+Victorloyf@users.noreply.github.com>
2017-11-26 01:04:50 -0500

    testing new user

--- php/GenerateQuery.php
+++ php/GenerateQuery.php
@@ -11,12 +11,12 @@ require_Once('php/dbconnect.php');
 class GenerateQuery
 {
     Public Static Function Create_New_User($user,$pass,$token){
-        this.$tsql = 'INSERT INTO Personal_Info (ID,Password,Token) VALUES('.$user.','.$pass.','.$token.');';
-        dbconnect::sql_insert(this.$tsql);
-        this.$tsql = 'INSERT INTO Location(ID)VALUES ('.$user.');';
-        dbconnect::sql_insert(this.$tsql);
-        this.$tsql = 'INSERT INTO STATUS(ID) VALUES ('.$user.');';
-        dbconnect::sql_insert(this.$tsql);
+        $new_tsql = 'INSERT INTO Personal_Info (ID,Password,Token) VALUES('.$user.','.$pass.','.$token.');';
+        dbconnect::sql_insert($new_tsql);
+        $new_tsql = 'INSERT INTO Location(ID)VALUES ('.$user.');';
+        dbconnect::sql_insert($new_tsql);
+        $new_tsql = 'INSERT INTO STATUS(ID) VALUES ('.$user.');';
+        dbconnect::sql_insert($new_tsql);
     }
 
 
