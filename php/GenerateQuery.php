1a125477569d4c216f5cba68149da2e0182d1483
Victorloyf <33768636+Victorloyf@users.noreply.github.com>
2017-11-26 01:28:11 -0500

    test kat

--- php/GenerateQuery.php
+++ php/GenerateQuery.php
@@ -13,10 +13,10 @@ class GenerateQuery
     Public Static Function Create_New_User($user,$pass,$token){
         $new_tsql = 'INSERT INTO Personal_Info (ID,Password,Token) VALUES('."'".$user."'".','."'".$pass."'".','."'".$token."'".');';
         dbconnect::sql_insert($new_tsql);
-        //$new_tsql = 'INSERT INTO Location(ID)VALUES ('.$user.');';
-        //dbconnect::sql_insert($new_tsql);
-        //$new_tsql = 'INSERT INTO STATUS(ID) VALUES ('.$user.');';
-        //dbconnect::sql_insert($new_tsql);
+        $new_tsql = 'INSERT INTO Location(ID)VALUES ('."'".$user."'".');';
+        dbconnect::sql_insert($new_tsql);
+        $new_tsql = 'INSERT INTO STATUS(ID) VALUES ('."'".$user."'".');';
+        dbconnect::sql_insert($new_tsql);
     }
 
 
