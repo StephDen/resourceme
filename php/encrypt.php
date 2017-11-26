6b12829399c9693165d6cf88540a09abfc24d4df
tolllord <xinghaozhaoex@gmail.com>
2017-11-25 19:56:30 -0500

    changing encryption

--- php/encrypt.php
+++ php/encrypt.php
@@ -13,7 +13,7 @@ class encrypt
     {
         // Using $salt makes it hard to guess how $checksum is generated
         // Caution: changing salt will invalidate all signed strings
-        $salt = "Simple salt";
+        $salt = "Mysalt";
         $checksum = sha1($string . $salt); // A random hash algorithm
         // return the string with the checksum at the end
         return $string . '--' . $checksum;
