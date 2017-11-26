cf80e1dfbe8316c0c8eb8ef224adf1f60ae84757
Victorloyf <33768636+Victorloyf@users.noreply.github.com>
2017-11-26 01:06:38 -0500

    seeing where it goes wrong

--- php/GenerateQuery.php
+++ php/GenerateQuery.php
@@ -13,10 +13,13 @@ class GenerateQuery
     Public Static Function Create_New_User($user,$pass,$token){
         $new_tsql = 'INSERT INTO Personal_Info (ID,Password,Token) VALUES('.$user.','.$pass.','.$token.');';
         dbconnect::sql_insert($new_tsql);
+        echo 1;
         $new_tsql = 'INSERT INTO Location(ID)VALUES ('.$user.');';
         dbconnect::sql_insert($new_tsql);
+        echo 2;
         $new_tsql = 'INSERT INTO STATUS(ID) VALUES ('.$user.');';
         dbconnect::sql_insert($new_tsql);
+        echo 3;
     }
 
 
