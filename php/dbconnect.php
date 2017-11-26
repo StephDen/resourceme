6a32801d0e3b29a6eb5437a0b2aa39b22636baa1
Victorloyf <33768636+Victorloyf@users.noreply.github.com>
2017-11-26 01:12:42 -0500

    commented error

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -46,7 +46,7 @@ class dbconnect {
       echo 'connected';
       $getResults = sqlsrv_query($conn, $tsql);
       if ($getResults == FALSE){       //checks if database completes query
-          echo (sqlsrv_errors());
+          //echo (sqlsrv_errors());
       }
   }
 
