042b40cf478dda59c105c74ecb229b7bbe7a5f77
tolllord <xinghaozhaoex@gmail.com>
2017-11-26 07:20:29 -0500

    creating search .php

---
+++ search.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 2017-11-26
+ * Time: 7:13 AM
+ */
+require_once('php/dbconnect.php');
+//POSTS
+
+//----------------------------------
+
+
+?>
