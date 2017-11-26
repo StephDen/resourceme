c8261c0af625aa74d49ffb47a699a47e18f353b5
Victorloyf <33768636+Victorloyf@users.noreply.github.com>
2017-11-26 03:12:56 -0500

    new thing

--- profileComplete.php
+++ profileComplete.php
@@ -10,23 +10,34 @@ echo "something";
 
 
 //convert all text boxes to variables
-$givenName = htmlspecialchars($_POST["givenName_register"]);
-$lastName = htmlspecialchars($_POST["lastName_register"]);
-$sex = htmlspecialchars($_POST["sex_register"]);
-$birthday = htmlspecialchars($_POST["birthday_register"]);
-$nationality = htmlspecialchars($_POST["nationality_register"]);
-$photo = htmlspecialchars($_POST["photo_register"]);
-$tel = htmlspecialchars($_POST["tel_register"]);
-$email = htmlspecialchars($_POST["email_register"]);
-$address1 = htmlspecialchars($_POST["address1_register"]);
-$address2 = htmlspecialchars($_POST["address2_register"]);
-$nationality2 = htmlspecialchars($_POST["nationality2_register"]);
-$postalCode = htmlspecialchars($_POST["postalCode_register"]);
-$martialStatus = htmlspecialchars($_POST["martialStatus_register"]);
-$religion = htmlspecialchars($_POST["religion_register"]);
-$ethnicity = htmlspecialchars($_POST["ethnicity_register"]);
-$education = htmlspecialchars($_POST["education_register"]);
-$occupation = htmlspecialchars($_POST["occupation_register"]);
+$givenName = htmlspecialchars($_POST["givenName"]);
+$lastName = htmlspecialchars($_POST["lastName"]);
+$sex = htmlspecialchars($_POST["sex"]);
+$birthday = htmlspecialchars($_POST["birthday"]);
+$Bcity = htmlspecialchars($_POST["Bcity"]);
+$Bcountry = htmlspecialchars($_POST["Bcountry"]);
+$nationality = htmlspecialchars($_POST["nationality"]);
+$photo = htmlspecialchars($_POST["photo"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$email = htmlspecialchars($_POST["email"]);
+$address1 = htmlspecialchars($_POST["address1"]);
+$address2 = htmlspecialchars($_POST["address2"]);
+$city = htmlspecialchars($_POST["city"]);
+$country = htmlspecialchars($_POST["country"]);
+$postalCode = htmlspecialchars($_POST["postalCode"]);
+$arrivalDate = htmlspecialchars($_POST["arrivalDate"]);
+$maritalStatus = htmlspecialchars($_POST["maritalStatus"]);
+$religion = htmlspecialchars($_POST["religion"]);
+$ethnicity = htmlspecialchars($_POST["ethnicity"]);
+$education = htmlspecialchars($_POST["education"]);
+$occupation = htmlspecialchars($_POST["occupation"]);
+$address1Origin = htmlspecialchars($_POST["address1Origin"]);
+$address2Origin = htmlspecialchars($_POST["address2Origin"]);
+$cityOrigin = htmlspecialchars($_POST["cityOrigin"]);
+$countryOrigin = htmlspecialchars($_POST["countryOrigin"]);
+$postalCodeOrigin = htmlspecialchars($_POST["postalCodeOrigin"]);
+$departureDate = htmlspecialchars($_POST["departureDate"]);
+
 
 
 //$tsql = ;
