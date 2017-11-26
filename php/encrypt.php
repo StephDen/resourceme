4ef575d80dcfbbd999720b9277ceea41e017d844
tolllord <xinghaozhaoex@gmail.com>
2017-11-25 20:51:26 -0500

    testing encryption

--- php/encrypt.php
+++ php/encrypt.php
@@ -71,7 +71,7 @@ class encrypt
      */
     public static function decrypt_string($iv_with_string) {
         //decoding the encryption
-        $iv_with_string = base64_decode($iv_with_string);
+        $string = base64_decode($iv_with_string);
         // Configuration (must match encryption)
         $cipher_type = MCRYPT_RIJNDAEL_256;
         $cipher_mode = MCRYPT_MODE_CBC;
@@ -79,8 +79,8 @@ class encrypt
         // Extract the initialization vector from the encrypted string.
         // The $iv comes before encrypted string and has fixed size.
         $iv_size = mcrypt_get_iv_size($cipher_type, $cipher_mode);
-        $iv = substr($iv_with_string, 0, $iv_size);
-        $encrypted_string = substr($iv_with_string, $iv_size);
+        $iv = substr($string, 0, $iv_size);
+        $encrypted_string = substr($string, $iv_size);
 
         $string = mcrypt_decrypt($cipher_type, $salt, $encrypted_string, $cipher_mode, $iv);
         return $string;
